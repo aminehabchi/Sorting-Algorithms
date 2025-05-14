a8bec52202111238cf980ba965fd979a428391de
aminehabchi <amine.habchi.01@gmail.com>
2025-05-14 22:41:14 +0100

    merge sort

--- rust/Sort/src/main.rs
+++ rust/Sort/src/main.rs
@@ -39,9 +39,44 @@ fn selection_sort(arr :&mut Vec<i32>){
     }
 }
 
-// fn merge_sort(arr1 :&mut Vec<i32>,arr1 :&mut Vec<i32>){
-    
-// }
+
+
+fn merge_sort(arr :&mut [i32])->Vec<i32>{
+    let l = arr.len();
+    if l <= 1 {
+        return arr.to_vec();
+    }
+
+    let left = merge_sort(&mut arr[0..l/2]);
+    let right = merge_sort(&mut arr[l/2..]);
+
+    let mut ar:Vec<i32>=Vec::new();
+
+    let mut i:usize=0;
+    let mut j:usize=0;
+
+    loop {
+        if i<left.len() && j<right.len(){
+            if left[i]<right[j]{
+                ar.push(left[i]);
+                i+=1;
+            }else{
+                ar.push(right[j]);
+                j+=1;
+            }
+        }else if i<left.len(){
+            ar.push(left[i]);
+            i+=1;
+        }else if j<right.len(){
+            ar.push(right[j]);
+            j+=1;
+        }else{
+            break
+        }
+    }
+
+    ar
+}
 
 fn quick_sort(arr :&mut [i32]){
     if arr.len()<=1{
@@ -69,11 +104,11 @@ fn quick_sort(arr :&mut [i32]){
 
 
 fn main() {
-    let mut arr:Vec<i32>=[5,1,2,0,6,9,4].to_vec();
+    let mut arr:Vec<i32>=[5,1,2,0,8,8,6,9,4].to_vec();
     
     println!("{:?}",arr);
 
-    quick_sort(&mut arr);
+    arr=merge_sort(&mut arr);
 
     println!("{:?}",arr);
 }
